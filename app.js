1d47441fb8dab15837ecb418af9e5b72cc1f9856
Marie ZAFIMEHY <38957@PST6904.sciences-po.fr>
2018-02-19 18:31:04 +0100

    Fin session 19/02

--- app.js
+++ app.js
@@ -1,1 +1,44 @@
-Reveal.initialize();
+Reveal.initialize({
+	controls: false,
+	controlsTutorial: true,
+	progress: true,
+	keyboard: true,
+	center: true,
+	touch: true,
+	mouseWheel: true,
+	width: 320,
+	height: 568,
+	center: false
+});
+
+var prenoms = ['Hana', 'Océane', 'Mélina', 'Laury Ann', 'Émilie', 'Awa', 'Aminata', 'Migna', 'Sena', 'Clarisse', 'George', 'Angela', 'Laura', 'Wendie'];
+var prenomsCurrent = 0;
+var prenomsInterval;
+
+function startPrenomsAnimation () {
+	prenomsInterval = setInterval(function() {
+		if (prenomsCurrent > prenoms.length - 1) {
+			prenomsCurrent = 0
+		}
+		document.getElementById('prenoms-animate').textContent = prenoms[prenomsCurrent]
+		prenomsCurrent++;
+	}, 150);
+}
+
+function stopPrenomsAnimation () {
+	clearInterval(prenomsInterval)
+}
+
+Reveal.addEventListener( 'ready', function( event ) {
+	if (Reveal.isFirstSlide()) {
+		startPrenomsAnimation()
+	}
+});
+
+Reveal.addEventListener( 'slidechanged', function( event ) {
+	if (Reveal.isFirstSlide()) {
+		startPrenomsAnimation()
+	} else if (prenomsInterval !== null) {
+		stopPrenomsAnimation()
+	}
+});
