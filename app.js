5afdd30ed0783c987cde40a11e790f90a3890b05
Marie Z <marie.zafimehy@sciencespo.fr>
2018-03-05 15:40:25 +0100

    image background

--- app.js
+++ app.js
@@ -21,23 +21,40 @@ function startPrenomsAnimation () {
 		}
 		document.getElementById('prenoms-animate').textContent = prenoms[prenomsCurrent]
 		prenomsCurrent++;
-	}, 150);
+	}, 400);
 }
 
 function stopPrenomsAnimation () {
 	clearInterval(prenomsInterval)
 }
 
+function findBackground(el) {
+	if(el.dataset) {
+		return el.dataset.backgroundImage || findBackground(el.parentNode)
+	} 
+	return null
+}
+
+function updateBackground(url) {
+	if (url !== null) {
+		document.querySelector('.reveal-bg').style.backgroundImage = 'url(' + url + ')'
+	} else {
+		document.querySelector('.reveal-bg').style.backgroundImage = null		
+	}
+}
+
 Reveal.addEventListener( 'ready', function( event ) {
 	if (Reveal.isFirstSlide()) {
 		startPrenomsAnimation()
+		updateBackground(findBackground(event.currentSlide))
 	}
 });
 
 Reveal.addEventListener( 'slidechanged', function( event ) {
-	if (Reveal.isFirstSlide()) {
+	if (event.indexh === 1 && event.indexv === 0) {
 		startPrenomsAnimation()
 	} else if (prenomsInterval !== null) {
 		stopPrenomsAnimation()
 	}
+	updateBackground(findBackground(event.currentSlide))
 });
